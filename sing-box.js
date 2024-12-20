eda9dba3475b63b62090013b84e6bf8b894f0bc8
GD2021 <81171294+GD2021@users.noreply.github.com>
2024-12-20 16:34:49 +0800

    Update sing-box.js

--- sing-box.js
+++ sing-box.js
@@ -1,11 +1,29 @@
-const { type, name } = $arguments;
+const fs = require('fs');
+const path = require('path');
+
+// 模拟 $arguments 和 $files
+const arguments = { type: 'subscription', name: 'my-proxies' };
+const files = [path.join(__dirname, 'config.json')];
+
+const { type, name } = arguments;
 const compatible_outbound = {
   tag: 'COMPATIBLE',
   type: 'direct',
 };
 
-let compatible;
-let config = JSON.parse($files[0]);
+let compatible = false;
+let config = JSON.parse(fs.readFileSync(files[0], 'utf8'));
+
+// 模拟 produceArtifact 函数
+async function produceArtifact(options) {
+  // 这里应该是获取代理节点的逻辑，这里用一个示例代替
+  return [
+    { tag: 'proxy1', type: 'http' },
+    { tag: 'proxy2', type: 'https' },
+    // 添加更多代理节点
+  ];
+}
+
 let proxies = await produceArtifact({
   name,
   type: /^1$|col/i.test(type) ? 'collection' : 'subscription',
@@ -15,18 +33,21 @@ let proxies = await produceArtifact({
 
 config.outbounds.push(...proxies);
 
-config.outbounds.map(i => {
-  if (['{all}'].includes(i.tag)) {
+config.outbounds.forEach(i => {
+  if (i.tag === '🐸 手动切换') {
     i.outbounds.push(...getTags(proxies));
   }
   if (['🇭🇰 香港节点', '🔯 香港自动'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies, /🇭🇰|HK|hk|香港|港|HongKong/i));
   }
-  if (['🇯🇵 日本节点'].includes(i.tag)) {
+  if (i.tag === '🇯🇵 日本节点') {
     i.outbounds.push(...getTags(proxies, /🇯🇵|JP|jp|日本|日|Japan/i));
   }
-  if (['🇺🇲 美国节点'].includes(i.tag)) {
+  if (i.tag === '🇺🇲 美国节点') {
     i.outbounds.push(...getTags(proxies, /🇺🇸|US|us|美国|美|United States/i));
+  }
+  if (i.tag === '♻️ 自动选择') {
+    i.outbounds.push(...getTags(proxies, /🇭🇰|HK|hk|香港|港|HongKong|🇯🇵|JP|jp|日本|日|Japan|🇺🇸|US|us|美国|美|United States/i));
   }
 });
 
@@ -40,7 +61,7 @@ config.outbounds.forEach(outbound => {
   }
 });
 
-$content = JSON.stringify(config, null, 2);
+fs.writeFileSync(path.join(__dirname, 'updated-config.json'), JSON.stringify(config, null, 2));
 
 function getTags(proxies, regex) {
   return (regex ? proxies.filter(p => regex.test(p.tag)) : proxies).map(p => p.tag);
