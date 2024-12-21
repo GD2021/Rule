e03e6e0db75504fd82878865b586fee8882e6a94
GD2021 <81171294+GD2021@users.noreply.github.com>
2024-12-21 15:19:18 +0800

    Update singbox.js

--- singbox.js
+++ singbox.js
@@ -1,29 +1,11 @@
-const fs = require('fs');
-const path = require('path');
-
-// 模拟 $arguments 和 $files
-const arguments = { type: 'subscription', name: 'my-proxies' };
-const files = [path.join(__dirname, 'config.json')];
-
-const { type, name } = arguments;
+const { type, name } = $arguments;
 const compatible_outbound = {
   tag: 'COMPATIBLE',
-  type: 'direct',
+  type: 'block',
 };
 
-let compatible = false;
-let config = JSON.parse(fs.readFileSync(files[0], 'utf8'));
-
-// 模拟 produceArtifact 函数
-async function produceArtifact(options) {
-  // 这里应该是获取代理节点的逻辑，这里用一个示例代替
-  return [
-    { tag: 'proxy1', type: 'http' },
-    { tag: 'proxy2', type: 'https' },
-    // 添加更多代理节点
-  ];
-}
-
+let compatible;
+let config = JSON.parse($files[0]);
 let proxies = await produceArtifact({
   name,
   type: /^1$|col/i.test(type) ? 'collection' : 'subscription',
@@ -31,25 +13,25 @@ let proxies = await produceArtifact({
   produceType: 'internal',
 });
 
-config.outbounds.push(...proxies);
+// console.log(config.outbounds);
 
-config.outbounds.forEach(i => {
-  if (i.tag === '🐸 手动切换') {
+config.outbounds.map(i => {
+  if ("outbounds" in i && i.outbounds.includes("{all}") && "filter" in i) {
+    i.outbounds = i.outbounds.filter(item => item != "{all}" && item != "block");
+    const p = getTags(proxies, i.filter[0].keywords[0]);
+    if (i.filter[0].action == "include") {
+      i.outbounds.push(...p);
+    } else if (i.filter[0].action == "exclude") {
+      i.outbounds.push(...(getTags(proxies).filter(item => !p.includes(item))));
+    }
+    delete i.filter;
+  } else if ("outbounds" in i && i.outbounds.includes("{all}") && !("filter" in i)) {
+    i.outbounds = i.outbounds.filter(item => item != "{all}");
     i.outbounds.push(...getTags(proxies));
   }
-  if (['🇭🇰 香港节点', '🔯 香港自动'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /🇭🇰|HK|hk|香港|港|HongKong/i));
-  }
-  if (i.tag === '🇯🇵 日本节点') {
-    i.outbounds.push(...getTags(proxies, /🇯🇵|JP|jp|日本|日|Japan/i));
-  }
-  if (i.tag === '🇺🇲 美国节点') {
-    i.outbounds.push(...getTags(proxies, /🇺🇸|US|us|美国|美|United States/i));
-  }
-  if (i.tag === '♻️ 自动选择') {
-    i.outbounds.push(...getTags(proxies, /🇭🇰|HK|hk|香港|港|HongKong|🇯🇵|JP|jp|日本|日|Japan|🇺🇸|US|us|美国|美|United States/i));
-  }
 });
+
+config.outbounds.push(...proxies);
 
 config.outbounds.forEach(outbound => {
   if (Array.isArray(outbound.outbounds) && outbound.outbounds.length === 0) {
@@ -61,8 +43,11 @@ config.outbounds.forEach(outbound => {
   }
 });
 
-fs.writeFileSync(path.join(__dirname, 'updated-config.json'), JSON.stringify(config, null, 2));
+$content = JSON.stringify(config, null, 2);
 
 function getTags(proxies, regex) {
+  if (regex) {
+    regex = new RegExp(regex);
+  }
   return (regex ? proxies.filter(p => regex.test(p.tag)) : proxies).map(p => p.tag);
 }
